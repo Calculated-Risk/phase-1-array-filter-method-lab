7a06e0b03a118b3be7c88ea04b7a75dc6c80050e
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-01 21:14:51 -0400

    passing lab so far

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+/* findMatching- This function takes an array of drivers' names and a string as arguments, 
+and returns the matching list of drivers. The function should be case insensitive.*/
+
+function findMatching(array, string){
+  return array.filter(name => name.toLowerCase() == string.toLowerCase());
+}
+
+//fuzzyMatch - This function takes an array of drivers' names and a string as arguments for querying 
+//the array, and returns all drivers whose names begin with the provided letters.
+
+function fuzzyMatch(array, string){
+  return array.filter(name => name.slice(0, 2) === string)
+}
+
